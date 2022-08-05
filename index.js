045908d1c51acc2a48c1dbb6094be37efaccd2a2
Ayush Vijayant <ayush.vijayant@gmail.com>
2022-08-06 00:40:30 +0530

    as per submission guidelines

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const http = require("http");
 const fs = require("fs");
+const readline = require("readline");
 
 let homeContent = "";
 let projectContent = "";
 let registrationFormContent = "";
+let jsContent = "";
 
 fs.readFile("home.html", (err, home) => {
   if (err) {
@@ -19,30 +21,51 @@ fs.readFile("project.html", (err, project) => {
   projectContent = project;
 });
 
-fs.readFile("registration.html", (err, data) => {
-  if (err) {
-    throw err;
-  }
-  registrationFormContent = data;
+const lineDetail = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
 });
 
-http
-  .createServer((request, response) => {
-    let url = request.url;
-    response.writeHeader(200, { "Content-Type": "text/html" });
-    switch (url) {
-      case "/project":
-        response.write(projectContent);
-        response.end();
-        break;
-      case "/registration":
-        response.write(registrationFormContent);
-        response.end();
-        break;
-      default:
-        response.write(homeContent);
-        response.end();
-        break;
-    }
-  })
-  .listen(3000);
+lineDetail.question(
+  "Enter path for registration form (registration.html) ",
+  (path) => {
+    fs.readFile(path, (err, data) => {
+      if (err) {
+        throw err;
+      }
+      registrationFormContent = data;
+    });
+
+    fs.readFile("main.js", (err, data) => {
+      if (err) {
+        throw err;
+      }
+      jsContent = data;
+    });
+    lineDetail.close();
+    http
+      .createServer((request, response) => {
+        let url = request.url;
+        response.writeHeader(200, { "Content-Type": "text/html" });
+        switch (url) {
+          case "/project":
+            response.write(projectContent);
+            response.end();
+            break;
+          case "/registration":
+            response.write(registrationFormContent);
+            response.end();
+            break;
+          case "/main.js":
+            response.write(jsContent);
+            response.end();
+            break;
+          default:
+            response.write(homeContent);
+            response.end();
+            break;
+        }
+      })
+      .listen(3000);
+  }
+);
