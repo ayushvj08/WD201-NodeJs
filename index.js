38863758654c2b8d3f1ee1c740fb84aed02bfd3a
Ayush Vijayant <ayush.vijayant@gmail.com>
2022-08-06 00:23:26 +0530

    add files for milestone

--- index.js
+++ index.js
@@ -1,5 +1,48 @@
-function hello() {
-  console.log("Hello NodeJs! Trying it for the first time.");
-}
+const http = require("http");
+const fs = require("fs");
 
-hello();
+let homeContent = "";
+let projectContent = "";
+let registrationFormContent = "";
+
+fs.readFile("home.html", (err, home) => {
+  if (err) {
+    throw err;
+  }
+  homeContent = home;
+});
+
+fs.readFile("project.html", (err, project) => {
+  if (err) {
+    throw err;
+  }
+  projectContent = project;
+});
+
+fs.readFile("registration.html", (err, data) => {
+  if (err) {
+    throw err;
+  }
+  registrationFormContent = data;
+});
+
+http
+  .createServer((request, response) => {
+    let url = request.url;
+    response.writeHeader(200, { "Content-Type": "text/html" });
+    switch (url) {
+      case "/project":
+        response.write(projectContent);
+        response.end();
+        break;
+      case "/registration":
+        response.write(registrationFormContent);
+        response.end();
+        break;
+      default:
+        response.write(homeContent);
+        response.end();
+        break;
+    }
+  })
+  .listen(3000);
